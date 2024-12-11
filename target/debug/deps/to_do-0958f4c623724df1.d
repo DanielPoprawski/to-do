50b81ba815d33ead86ebf50176c57a4747a7366a
DPopraw <danielbpoprawski@gmail.com>
2024-12-10 20:32:08 -0500

    Standard

--- target/debug/deps/to_do-0958f4c623724df1.d
+++ target/debug/deps/to_do-0958f4c623724df1.d
@@ -1,5 +1,5 @@
-e:\to-do\target\debug\deps\libto_do-0958f4c623724df1.rmeta: src/main.rs
+E:\to-do\target\debug\deps\libto_do-0958f4c623724df1.rmeta: src/main.rs
 
-e:\to-do\target\debug\deps\to_do-0958f4c623724df1.d: src/main.rs
+E:\to-do\target\debug\deps\to_do-0958f4c623724df1.d: src/main.rs
 
 src/main.rs:
